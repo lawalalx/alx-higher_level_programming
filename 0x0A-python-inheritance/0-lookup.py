e181b9314730b53240b72eeb8ad01ac7152d6036
lawalalx <lawal.alx@gmail.com>
2024-02-06 09:22:08 -0800

    test

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+    
+    :param obj: The object to inspect.
+    :return: A list of attributes and methods.
+    """
+    return dir(obj)
