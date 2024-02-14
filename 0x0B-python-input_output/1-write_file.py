062b62b156d863c640a832c96e8181f2e00747c2
lawalalx <lawal.alx@gmail.com>
2024-02-14 18:50:05 +0100

    test

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,11 +4,8 @@ def write_file(filename="", text=""):
     """
         Write a function that writes a string to a text file (UTF8) and returns the number of characters written
     """
-    count = 0
     with open(filename, mode='w', encoding='utf-8') as file:
         file.write(text)
     file.close()
-    for word in text:
-        for character in word:
-            count += 1
+    count = sum(len(word) for word in text)
     return (count)
