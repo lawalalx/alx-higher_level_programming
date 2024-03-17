cd0244d8531900d3eb850eb57578dc65767cbf90
Lawal Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-03-17 16:08:41 +0100

    test

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,8 @@
 -- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT cities.name
-FROM cities
-INNER JOIN states ON cities.state_id = states.id
-WHERE states.name = 'California'
-ORDER BY cities.id;
+SELECT `id`, `name`
+FROM `cities`
+WHERE `state_id` IN
+      (SELECT `id`
+  FROM `states`
+  WHERE `name` = "California")
+ORDER BY `id`;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- Write a script that lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.name, states.name
-FROM cities
-LEFT JOIN states ON cities.state_id = states.id
-ORDER BY cities.id;
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
